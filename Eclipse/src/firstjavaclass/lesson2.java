05d6c951b6f9ce0b53d9d1ab21c222cc9fa9e599
Holeg777 <o.havrun@gmail.com>
2020-11-19 17:25:12 +0000

    adding java test cases in th ehub

---
+++ Eclipse/src/firstjavaclass/lesson2.java
@@ -0,0 +1,13 @@
+package firstjavaclass;
+
+public class lesson2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hellow World!");
+    System.out.println("Selenium Training");
+	
+	}
+
+}
